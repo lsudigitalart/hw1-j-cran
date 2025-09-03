65cba46680b3777001d1018bb427d32f484929cf
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-09-03 10:51:19 -0500

    halfpipe + dots

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    ellipse(0, 10, 700, 700);
+    fill(0);
+    ellipse(40, 35, 500, 500);
+
+    fill(0);
+    ellipse(0,350,30,30);
+    fill(0);
+    ellipse(90,335,30,30);
+    fill(0);
+    ellipse(170,300,30,30);
+    fill(0);
+    ellipse(235,250,30,30);
+}
